28388ebd5fecc7d40e7a6110178a8a852140f83b
jjtt <3908945+jjtt@users.noreply.github.com>
2021-10-10 19:10:09 +0300

    dummy db content

---
+++ init/init.sql
@@ -0,0 +1,38 @@
+create table m (id varchar(10), sessio varchar(10), osallistuja varchar(10));
+
+insert into m values ('1', 'game99.1', 'aj');
+insert into m values ('2', 'game99.2', 'aj');
+insert into m values ('3', 'game99.3', 'aj');
+insert into m values ('4', 'game99.4', 'aj');
+insert into m values ('5', 'game00.1', 'aj');
+insert into m values ('6', 'game00.2', 'aj');
+insert into m values ('7', 'game00.3', 'aj');
+insert into m values ('8', 'game00.4', 'aj');
+
+--insert into m values ('11', 'game99.1', 'mr');
+insert into m values ('12', 'game99.2', 'mr');
+insert into m values ('13', 'game99.3', 'mr');
+insert into m values ('14', 'game99.4', 'mr');
+insert into m values ('15', 'game00.1', 'mr');
+insert into m values ('16', 'game00.2', 'mr');
+insert into m values ('17', 'game00.3', 'mr');
+--insert into m values ('18', 'game00.4', 'mr');
+
+insert into m values ('21', 'game99.1', 'bu');
+insert into m values ('22', 'game99.2', 'bu');
+insert into m values ('23', 'game99.3', 'bu');
+--insert into m values ('24', 'game99.4', 'bu');
+insert into m values ('25', 'game00.1', 'bu');
+insert into m values ('26', 'game00.2', 'bu');
+insert into m values ('27', 'game00.3', 'bu');
+insert into m values ('28', 'game00.4', 'bu');
+
+--insert into m values ('31', 'game99.1', 'ce');
+--insert into m values ('32', 'game99.2', 'ce');
+--insert into m values ('33', 'game99.3', 'ce');
+--insert into m values ('34', 'game99.4', 'ce');
+insert into m values ('35', 'game00.1', 'ce');
+insert into m values ('36', 'game00.2', 'ce');
+--insert into m values ('37', 'game00.3', 'ce');
+--insert into m values ('38', 'game00.4', 'ce');
+
