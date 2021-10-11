234cc4331356e849ca26adb063f43d0a86db5924
jjtt <3908945+jjtt@users.noreply.github.com>
2021-10-11 08:24:02 +0300

    Use mariadb 10.0

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -ti --rm -v $(pwd)/init:/docker-entrypoint-initdb.d:Z -v $(pwd):/foo:Z -p 3306:3306  --name some-mariadb -e MARIADB_DATABASE=game -e MARIADB_ROOT_PASSWORD=my-secret-pw mariadb:10
+docker run -ti --rm -v $(pwd)/init:/docker-entrypoint-initdb.d:Z -v $(pwd):/foo:Z -p 3306:3306  --name some-mariadb -e MYSQL_DATABASE=game -e MYSQL_ROOT_PASSWORD=my-secret-pw mariadb:10.0
